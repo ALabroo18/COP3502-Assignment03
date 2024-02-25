78b54071e0b79e6227e1c09b7e2fac42e2762c7b
Anish Labroo <anishlabroo@gmail.com>
2024-02-24 22:16:20 -0500

    Added loop for scanning people

--- main.c
+++ main.c
@@ -5,14 +5,22 @@ typedef struct test{
 }teststr;
 int main(){
     int persCount;
+    //char name[20][persCount];
     char name[20][persCount];
-       
-    printf(" \n");
+    
+    scanf("%d",&persCount);
+
+    //Make a for loop that scans each people's name
+    for(int i = 0; i < persCount; i++){
+        for(int j = 0; j < 20; j--){
+            scanf("%c", &name[i][j]);
+            printf("%c", name[i][j]);
+        }
+        printf("\n");
+    }
     //while 
     //persCount++
     //scanf("%c", [persCount - 1][0]);
     printf("s");
-    printf("s");
-//werewrr
     return 0;
 }
